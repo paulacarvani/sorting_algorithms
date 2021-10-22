520d625c8614d5f7977df308b9873b8c5d973fc3
paulacarvani <3312@holbertonschool.com>
2021-10-22 16:04:40 -0500

    First files wit task 0

---
+++ 0-bubble_sort.c
@@ -0,0 +1,35 @@
+#include "sort.h"
+
+/**
+ * bubble_sort - sorts an array of integers in ascending order
+ * @array: array of integers
+ * @size: size of the array
+ */
+
+void bubble_sort(int *array, size_t size)
+{
+	size_t i, swapped;
+	int temp;
+
+	if (size == 0 || array == NULL)
+	{
+		return;
+	}
+
+	swapped = 1;
+	while (swapped)
+	{
+		swapped = 0;
+		for (i = 0; i < size -1; i++)
+		{
+			if (array[i] > array[i + 1])
+			{
+				temp = array[i + 1];
+				array[i + 1] = array[i];
+				array[i] = temp;
+				swapped = 1;
+				print_array(array, size);
+			}
+		}
+	}
+}
