19e7ca89b980b4314bc89fab49dcbb9728591f50
paulacarvani <3312@holbertonschool.com>
2021-10-22 16:07:23 -0500

    Correction BEtty style

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -20,7 +20,7 @@ void bubble_sort(int *array, size_t size)
 	while (swapped)
 	{
 		swapped = 0;
-		for (i = 0; i < size -1; i++)
+		for (i = 0; i < size - 1; i++)
 		{
 			if (array[i] > array[i + 1])
 			{
